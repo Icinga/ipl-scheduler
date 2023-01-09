497d3c4a94052832bfc034bb73cfb848c1edaae7
Yonas Habteab <yonas.habteab@icinga.com>
2023-01-09 09:58:00 +0100

    Introduce `RRule` class

---
+++ src/RRule.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace ipl\Scheduler;
+
+use BadMethodCallException;
+use DateTime;
+use DateTimeInterface;
+use Generator;
+use ipl\Scheduler\Contract\Frequency;
+use Recurr\Rule as RecurrRule;
+use Recurr\Transformer\ArrayTransformer;
+use Recurr\Transformer\ArrayTransformerConfig;
+use Recurr\Transformer\Constraint\AfterConstraint;
+use Recurr\Transformer\Constraint\BetweenConstraint;
+
+use function ipl\Stdlib\get_php_type;
+
+class RRule implements Frequency
+{
+    /** @var string Run once a year */
+    public const YEARLY = 'YEARLY';
+
+    /** @var string Run every 3 month starting from the given start time */
+    public const QUARTERLY = 'QUARTERLY';
+
+    /** @var string Run once a month */
+    public const MONTHLY = 'MONTHLY';
+
+    /** @var string Run once a week based on the specified start time */
+    public const WEEKLY = 'WEEKLY';
+
+    /** @var string Run once a day at the specified start time */
+    public const DAILY = 'DAILY';
+
+    /** @var string Run once an hour */
+    public const HOURLY = 'HOURLY';
+
+    /** @var string Run once a minute */
+    public const MINUTELY = 'MINUTELY';
+
+    /** @var RecurrRule */
+    protected $rrule;
+
+    /** @var ArrayTransformer */
+    protected $transformer;
+
+    public function __construct(string $rule, DateTimeInterface $start)
+    {
+        $this->rrule = new RecurrRule($rule);
+        $this->transformer = new ArrayTransformer();
+
+        $this->startAt($start);
+        $this->limitTransformer(3);
+    }
+
+    public function isDue(DateTime $dateTime): bool
+    {
+        if ($dateTime < $this->rrule->getStartDate() || $this->isExpired($dateTime)) {
+            return false;
+        }
+
+        $nextDue = $this->getNextRecurrences($dateTime);
+        if (! $nextDue->valid()) {
+            return false;
+        }
+
+        return $nextDue->current() == $dateTime;
+    }
+
+    public function getNextDue(DateTime $dateTime): DateTime
+    {
+        if ($this->isExpired($dateTime)) {
+            return $this->rrule->getEndDate();
+        }
+
+        $nextDue = $this->getNextRecurrences($dateTime, false);
+        if (! $nextDue->valid()) {
+            return $dateTime;
+        }
+
+        return $nextDue->current();
+    }
+
+    public function isExpired(DateTime $dateTime): bool
+    {
+        $end = $this->rrule->getEndDate();
+        if (! $end || $end == $this->rrule->getStartDate()) {
+            return false;
+        }
+
+        return $end < $dateTime;
+    }
+
+    /**
+     * Set the start time of this frequency
+     *
+     * @param DateTimeInterface $start
+     *
+     * @return $this
+     */
+    public function startAt(DateTimeInterface $start): self
+    {
+        $startDate = clone $start;
+        // When the start time contains microseconds, the first recurrence will always be skipped, as
+        // the transformer operates only up to seconds level. See also the upstream issue #155
+        $startDate->setTime($start->format('H'), $start->format('i'), $start->format('s'));
+        $this->rrule->setStartDate($startDate, true);
+
+        return $this;
+    }
+
+    /**
+     * Set the end time of this frequency
+     *
+     * @param DateTimeInterface $end
+     *
+     * @return $this
+     */
+    public function endAt(DateTimeInterface $end): self
+    {
+        $this->rrule->setEndDate($end);
+
+        return $this;
+    }
+
+    /**
+     * Get the RRULE as a string
+     *
+     * @return string
+     */
+    public function getRuleString(): string
+    {
+        return $this->rrule->getString();
+    }
+
+    /**
+     * Get the frequency of this rule
+     *
+     * @return string
+     */
+    public function getRepeat(): string
+    {
+        return $this->rrule->getFreqAsText();
+    }
+
+    /**
+     * Get a set of recurrences relative to the given time and bounded to the configured generator's limit
+     *
+     * @param DateTimeInterface $dateTime
+     * @param bool $include Whether to include the passed time in the result set
+     *
+     * @return Generator
+     */
+    public function getNextRecurrences(DateTimeInterface $dateTime, bool $include = true): Generator
+    {
+        if (! $this->rrule->getEndDate()) {
+            $constraint = new AfterConstraint($dateTime, $include);
+        } else {
+            $constraint = new BetweenConstraint($dateTime, $this->rrule->getEndDate(), $include);
+        }
+
+        // Setting the start date to a date time smaller than now causes the underlying library
+        // not to generate any recurrences when using the regular frequencies such as `MINUTELY` etc.
+        // and the `$countConstraintFailures` is set to true. We need also to tell the transformer
+        // not to count the recurrences that fail the constraint's test!
+        $recurrences = $this->transformer->transform($this->rrule, $constraint, false);
+        foreach ($recurrences as $recurrence) {
+            yield $recurrence->getStart();
+        }
+    }
+
+    /**
+     * Limit the underlying recurrence generator to produce only up to the passed limit
+     *
+     * @param int $limit
+     *
+     * @return $this
+     */
+    public function limitTransformer(int $limit): self
+    {
+        // Limit the generated recurrences to the provided counts, otherwise, the transformer will
+        // generate meaningless "732" recurrences each time by default
+        $config = new ArrayTransformerConfig();
+        $config->setVirtualLimit($limit);
+        $this->transformer->setConfig($config);
+
+        return $this;
+    }
+
+    /**
+     * Redirect all public method calls to the underlying rrule object
+     *
+     * @param string $methodName
+     * @param array $args
+     *
+     * @return mixed
+     *
+     * @throws BadMethodCallException If the given method doesn't exist
+     */
+    public function __call(string $methodName, array $args)
+    {
+        if (! method_exists($this->rrule, $methodName)) {
+            throw new BadMethodCallException(
+                sprintf('Call to undefined method %s::%s()', get_php_type($this->rrule), $methodName)
+            );
+        }
+
+        return call_user_func_array([$this->rrule, $methodName], $args);
+    }
+}
