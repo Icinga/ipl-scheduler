353fab6f25757a28d4b8f1550106f6bb2ba4cd4d
Johannes Rauh <johannes.rauh@icinga.com>
2026-03-19 16:26:33 +0100

    fixup! Introduce `FrequencyStatus` enum for frequency lifecycle state
    Adjust the comments and punctuation in `FrequencyStatus`.
    
    This commit will be squashed into the original commit.

--- src/Common/FrequencyStatus.php
+++ src/Common/FrequencyStatus.php
@@ -13,14 +13,14 @@ enum FrequencyStatus
     /** The frequency has a start date which has not been reached yet */
     case PENDING;
 
-    /** The start date has already been reached, and the end date has not been reached yet. */
+    /** The start date has already been reached, and the end date has not been reached yet */
     case READY;
 
     /** The frequency has an end date which has already been reached */
     case EXPIRED;
 
     /**
-     * Get the frequency status of the frequency to determine if it's ready to run.
+     * Get the frequency status of the frequency to determine if it's ready to run
      *
      * @param Frequency $frequency
      * @param DateTimeInterface $dateTime The reference time to check the frequency's start and end dates against
@@ -29,19 +29,16 @@ enum FrequencyStatus
      */
     public static function fromFrequency(Frequency $frequency, DateTimeInterface $dateTime): self
     {
-        // If the end date of the frequency has already been reached, it's expired.
-        // If the end date is null, it can't expire.
+        // If the frequency has no end date, it will never expire.
         if ($frequency->getEnd() !== null && $frequency->getEnd() < $dateTime) {
             return self::EXPIRED;
         }
 
-        // If the start date of the frequency has not been reached yet, it's pending.
-        // If the start date is null, it has no lower bound and is instantly ready.
+        // If the frequency has no start date, it will never be pending.
         if ($frequency->getStart() !== null && $frequency->getStart() > $dateTime) {
             return self::PENDING;
         }
 
-        // If none of the earlier checks have been triggered, the frequency is ready to run.
         return self::READY;
     }
 
